f55380aad0082d4b6e723af1620083a40d4750fb
Omoyajowo Temidayo <temideewan@gmail.com>
2023-05-17 07:59:12 +0100

    finish location part of rent modal

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -27,6 +27,11 @@ const Map: React.FC<MapProps> = ({
     className="h-[35vh] rounded-lg"
     >
        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' />
+       {center && (
+        <Marker 
+        position={center as L.LatLngExpression}
+        />
+       )}
     </MapContainer>
    );
 }
