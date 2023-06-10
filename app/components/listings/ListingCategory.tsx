061271583c44fc7d48ed34fd51873ece98c5e087
Omoyajowo Temidayo <temideewan@gmail.com>
2023-06-10 08:35:45 +0100

    complete first iteration of individual listing details page

---
+++ app/components/listings/ListingCategory.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { IconType } from "react-icons";
+
+interface ListingCategoryProps {
+  icon: IconType;
+  label: string;
+  description: string;
+}
+const ListingCategory: React.FC<ListingCategoryProps> = ({
+  icon: Icon,
+  label,
+  description,
+
+}) => {
+  return (<div className="flex flex-col gap-6">
+    <div className="flex flex-row items-center gap-4">
+      <Icon size={40} className="text-neutral-600" />
+      <div className="flex flex-col">
+        <p className="text-lg font-semibold">
+          {label}
+        </p>
+        <p className="text-neutral-500 font-light">
+          {description}
+        </p>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+export default ListingCategory;
