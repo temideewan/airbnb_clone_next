ddde9d32a2e0bbd93cd63e54842333bf425e55a9
Omoyajowo Temidayo <temideewan@gmail.com>
2023-06-15 21:09:33 +0100

    complete search filter for listings

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -9,6 +9,8 @@ import { Modal } from "./Modal";
 import useSearchModal from "@/app/hooks/useSearchModal";
 import { formatISO } from "date-fns";
 import Heading from "../Heading";
+import Calendar from "../inputs/Calendar";
+import Counter from "../inputs/Counter";
 
 enum STEPS {
   LOCATION = 0,
@@ -97,15 +99,62 @@ const SearchModal = () => {
       value={location}
       onChange={(value) => setLocation(value as CountrySelectValue)}
       />
+      <hr />
+      {location?.latlng && <Map center={location?.latlng} />}
     </div>
   )
+
+  if(step === STEPS.DATE){
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading 
+      title="When do you plan to go"
+      subtitle="Make sure everyone is free!"
+      />
+      <Calendar 
+      value={dateRange}
+      onChange={(value) =>setDateRange(value.selection)}
+      />
+      </div>
+    )
+  }
+  if(step === STEPS.INFO){
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading 
+      title="More Information"
+      subtitle="Find your perfect place!"
+      />
+      <Counter 
+      title="Guests"
+      subtitle="How many guests are coming?"
+      value={guestCount}
+      onChange={(value) => setGuestCount(value)}
+      />
+      <Counter 
+      title="Rooms"
+      subtitle="How many rooms do you need?"
+      value={roomCount}
+      onChange={(value) => setRoomCount(value)}
+      />
+      <Counter 
+      title="Bathrooms"
+      subtitle="How many bathrooms do you need?"
+      value={bathroomCount}
+      onChange={(value) => setBathroomCount(value)}
+      />
+      </div>
+    )
+  }
   return (
     <Modal
       isOpen={searchModal.isOpen}
       onClose={searchModal.onClose}
-      onSubmit={searchModal.onOpen}
+      onSubmit={onSubmit}
       title="Filters"
-      actionLabel="Search"
+      actionLabel={actionLabel}
+      secondaryAction={step === STEPS.LOCATION? undefined: onBack}
+      secondaryActionLabel={secondaryActionLabel}
       body={bodyContent}
     />
   );
