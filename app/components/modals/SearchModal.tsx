e31781f3a1138e35549e3e781712ef185e8a10b6
Omoyajowo Temidayo <temideewan@gmail.com>
2023-06-18 20:17:09 +0100

    fix lint issue on project wrap up

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -35,6 +35,7 @@ const SearchModal = () => {
 
   const Map = useMemo(() => dynamic(() => import("../Map"), {
     ssr: false
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }), [location])
 
   const onBack = useCallback(() => {
